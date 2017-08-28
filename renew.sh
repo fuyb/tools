c586fef24fc8371ddd04fa66d8c74dace9c4ed21
fuyanbin <been1986@gmail.com>
2017-08-28 23:05:16 +0800

    general keystore

--- renew.sh
+++ renew.sh
@@ -1,9 +1,55 @@
+#!/bin/bash
 # Renew Let's Encrypt SSL cert
+set -x
+
+DOMAIN="ssl.example.com"
+DOMAIN2="ssl.example.com"
+NAME="example"
+PASSWD="password"
+LETSENCRYPT_PATH="/etc/letsencrypt/live/$DOMAIN"
 
 cd /opt/letsencrypt/
-./letsencrypt-auto --config /etc/letsencrypt/cli.ini -d yanbin.me -d pic.yanbin.me \
-     -d img.yanbin.me -d blog.yanbin.me -d wiki.yanbin.me -d paste.yanbin.me \
-     -d zzio.org -d otog.cc --email been1986@gmail.com certonly
+./letsencrypt-auto certonly \
+     --config /etc/letsencrypt/webroot.ini \
+     -d $DOMAIN \
+     -d $DOMAIN2
+
+openssl pkcs12 -export \
+  -in $LETSENCRYPT_PATH/fullchain.pem \
+  -inkey $LETSENCRYPT_PATH/privkey.pem \
+  -out $LETSENCRYPT_PATH/fullchain_and_key.p12 \
+  -name $NAME -password pass:$PASSWD
+
+keytool -delete \
+  -alias $NAME \
+  -keystore $LETSENCRYPT_PATH/keystore.jks \
+  -storepass $PASSWD \
+  -noprompt
+
+keytool -importkeystore \
+  -deststorepass $PASSWD \
+  -destkeypass $PASSWD \
+  -destkeystore $LETSENCRYPT_PATH/keystore.jks \
+  -srckeystore $LETSENCRYPT_PATH/fullchain_and_key.p12 \
+  -srcstoretype PKCS12 \
+  -srcstorepass $PASSWD \
+  -alias $NAME
+
+keytool -export -alias $NAME \
+  -file $LETSENCRYPT_PATH/$NAME.cer \
+  -keystore $LETSENCRYPT_PATH/keystore.jks \
+  -storepass $PASSWD -noprompt
+
+keytool -delete \
+  -alias $NAME \
+  -keystore $LETSENCRYPT_PATH/truststore.jks \
+  -storepass $PASSWD \
+  -noprompt
+
+keytool -import -trustcacerts -alias $NAME \
+  -file $LETSENCRYPT_PATH/$NAME.cer \
+  -keystore $LETSENCRYPT_PATH/truststore.jks \
+  -storepass $PASSWD -noprompt
 
 if [ $? -ne 0 ]
  then
@@ -11,7 +57,7 @@ if [ $? -ne 0 ]
         echo -e "The Lets Encrypt Cert has not been renewed! \n \n" $ERRORLOG | \
                mail -s "Lets Encrypt Cert Alert" been1986@gmail.com
 else 
-        systemctl reload nginx
+        systemctl restart red5pro
 fi
 
 exit 0
